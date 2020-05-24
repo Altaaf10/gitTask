054c15b8d3c1863616cac198c4e02d6cd7b66d9b
altaaf <altaaf10@gmail.com>
2020-05-24 21:07:43 +0200

    include user input helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,14 @@
-
+import java.util.Scanner;
 public class helloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Git is Awesome!");
+		Scanner s = new Scanner (System.in);
+		 
+		System.out.println("Input the data that you would like to display");
+		String input = s.next();
+		
+		System.out.println(input);
+		
 	}
 
 }
